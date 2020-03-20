709b0684ff094583db60f603cd856837c460a2ff
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2020-03-20 13:44:54 +0900

    Playerの精製方法を修正

---
+++ Assets/Scripts/SpawnActorOnStart.cs
@@ -0,0 +1,23 @@
+﻿using HK.Framework.EventSystems;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.STG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class SpawnActorOnStart : MonoBehaviour
+    {
+        [SerializeField]
+        private ActorSpawner spawner = default;
+
+        [SerializeField]
+        private Transform spawnPosition = default;
+
+        void Start()
+        {
+            this.spawner.Spawn(this.spawnPosition.position, this.spawnPosition.rotation);
+        }
+    }
+}
