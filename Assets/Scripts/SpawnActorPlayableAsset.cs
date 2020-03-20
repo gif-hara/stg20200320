7eb3120dbd92c9ef17582a8900d658ab75b5fb10
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2020-03-20 14:37:13 +0900

    TimelineでActorを生成できるように

---
+++ Assets/Scripts/SpawnActorPlayableAsset.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace HK.STG
+{
+    [System.Serializable]
+    public class SpawnActorPlayableAsset : PlayableAsset
+    {
+        public List<Element> elements = default;
+
+        // Factory method that generates a playable based on this asset
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
+        {
+            var behaviour = new SpawnActorPlayableBehaviour();
+            behaviour.Initialize(this.elements);
+
+            return ScriptPlayable<SpawnActorPlayableBehaviour>.Create(graph, behaviour);
+        }
+
+        [Serializable]
+        public class Element
+        {
+            [SerializeField]
+            private Vector3 spawnPosition;
+
+            [SerializeField]
+            private ActorSpawner spawner;
+
+            public void Spawn()
+            {
+                this.spawner.Spawn(this.spawnPosition, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnActorPlayableBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace HK.STG
+{
+    // A behaviour that is attached to a playable
+    public class SpawnActorPlayableBehaviour : PlayableBehaviour
+    {
+        private List<SpawnActorPlayableAsset.Element> elements;
+
+        public void Initialize(List<SpawnActorPlayableAsset.Element> elements)
+        {
+            this.elements = elements;
+        }
+
+        // Called when the state of the playable is set to Play
+        public override void OnBehaviourPlay(Playable playable, FrameData info)
+        {
+            foreach(var e in this.elements)
+            {
+                e.Spawn();
+            }
+        }
+    }
+}
